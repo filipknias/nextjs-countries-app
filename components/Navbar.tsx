cf5f18a4385eec777caf553e818eff835320e40c
Filip <filip.knias@o2.pl>
2022-09-21 15:27:01 +0200

    add link to home page in navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import { BsMoon, BsSun } from 'react-icons/bs';
 import useDarkTheme from '../hooks/useDarkTheme';
+import Link from 'next/link';
 
 const Navbar: FC = () => {
   const { darkTheme, setDarkTheme } = useDarkTheme();
@@ -8,7 +9,9 @@ const Navbar: FC = () => {
   return (
     <div className="w-full bg-white dark:bg-gray-700 shadow-md">
       <div className="container mx-auto p-5 flex items-center justify-between dark:text-white text-black">
-        <h1 className="font-bold md:text-2xl">Where in the world?</h1>
+        <Link href="/">
+          <h1 className="font-bold md:text-2xl cursor-pointer">Where in the world?</h1>
+        </Link>
         <div className="flex items-center gap-2 cursor-pointer md:text-xl" onClick={() => setDarkTheme(!darkTheme)}>
           {darkTheme ? (
             <>
